332dbaaac00be0e91c481e660770b92990126c1b
Souvikns <souvikde.ns@gmail.com>
2022-01-09 00:19:29 +0530

    feat: create notification when new user joins

--- src/bot/index.ts
+++ src/bot/index.ts
@@ -1,9 +1,10 @@
 import { Probot } from 'probot';
+import Axios from 'axios';
 
 function bot(app: Probot) {
   app.on('issues.opened', async (context) => {
     const issueComment = context.issue({
-      body: 'Thanks for opening this issue!',
+      body: `Thanks for opening this issue! ${process.env.GHACTION}`,
     });
     await context.octokit.issues.createComment(issueComment);
   });
@@ -12,6 +13,17 @@ function bot(app: Probot) {
 
   // To get your app running against GitHub, see:
   // https://probot.github.io/docs/development/
+
+
+  app.on('organization.member_added', async (context) => {
+    await Axios({
+      url: process.env.DISCORD_NEW_USER,
+      method: 'POST',
+      data: {
+        content: `${context.payload.membership.user.login} joined the Organization`
+      }
+    })
+  })
 }
 
 export default bot;
