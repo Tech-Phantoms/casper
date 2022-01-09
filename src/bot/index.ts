0f1b1eebe0996fe4c7b5cb67594bbe4db05894bb
Souvikns <souvikde.ns@gmail.com>
2022-01-09 19:08:18 +0530

    minor fix

--- src/bot/index.ts
+++ src/bot/index.ts
@@ -4,7 +4,7 @@ import Axios from 'axios';
 function bot(app: Probot) {
   app.on('issues.opened', async (context) => {
     const issueComment = context.issue({
-      body: `Thanks for opening this issue! `,
+      body: 'Thanks for opening this issue!',
     });
     await context.octokit.issues.createComment(issueComment);
   });
